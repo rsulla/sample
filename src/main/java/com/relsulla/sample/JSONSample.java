2d3b26c73cb31767a370d1608115cb8109749ac0
Bob Sulla <Bob Sulla>
2015-11-07 22:19:48 -0600

    Initial commit

---
+++ src/main/java/com/relsulla/sample/JSONSample.java
@@ -0,0 +1,28 @@
+package com.relsulla.sample;
+
+
+import org.json.JSONObject;
+
+/**
+ * Created by Bob on 11/7/2015.
+ */
+public class JSONSample {
+    public static void main(String[] args) {
+
+        JSONSample instance = new JSONSample();
+
+        instance.run();
+    }
+
+    private void run() {
+
+        JSONObject obj = new JSONObject();
+
+        obj.put("name", "foo");
+        obj.put("num", new Integer(100));
+        obj.put("balance", new Double(1000.21));
+        obj.put("is_vip", new Boolean(true));
+
+        System.out.print(obj);
+    }
+}
